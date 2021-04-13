494d1486a45fb139f2a91a26d4e517038d1b0122
Sheyla V Perez <42497841+sheylavperez@users.noreply.github.com>
2021-04-12 21:38:07 -0400

    Updated Start Scene
    I added the background image and got one of the button to work

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+
+    }
+
+    // Update is called once per frame
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit!");
+    }
+}
